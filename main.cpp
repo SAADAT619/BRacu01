9e4f461f54f5ee9b19d3c354f880cfdacb8ecac5
MD SAMIN YASAR SAADAT <>
2021-04-07 03:51:13 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n,m;
+    cin >> n >> m; //n=number of tickets ,,,, m=number of customer
+
+    map<int, int> tickets;
+
+    for(int i=0; i<n; i++){
+            int t;
+            cin >> t; 
+    }
+
+    return 0;
+}
