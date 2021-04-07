5e5714a15497b955e47ed33a56ef8601144bd8fd
MD SAMIN YASAR SAADAT <>
2021-04-07 03:56:25 +0000

    new changes

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+//     ios::sync_with_stdio(false);
+//     cin.tie(0); cout.tie(0);
 
-    int n,m;
-    cin >> n >> m; //n=number of tickets ,,,, m=number of customer
+//     int n,m;
+//     cin >> n >> m; //n=number of tickets ,,,, m=number of customer
 
-    map<int, int> tickets;
+//     map<int, int> tickets;
 
-    for(int i=0; i<n; i++){
-            int t;
-            cin >> t; 
-    }
+//     for(int i=0; i<n; i++){
+//             int t;
+//             cin >> t; 
+//     }
+
+cout << "Hello Saadat" << endl;
 
     return 0;
 }
