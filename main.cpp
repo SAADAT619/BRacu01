403127a954ff476d395fcf869399a3dcd3390225
MD SAMIN <>
2021-04-09 09:40:11 +0000

    new changes

--- main.cpp
+++ main.cpp
@@ -2,30 +2,23 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    int n,m;
-    cin >> n >> m; //n=number of tickets ,,,, m=number of customer
-
-    map<int, int> tickets;
-
-    for(int i=0; i<n; i++){
-            int t;
-            cin >> t; 
-            tickets[t]++;
-    }
-
-    for(int i = 0; i<m; i++){
-            int price;
-            cin >> price;
-            auto it = tickets.upper_bound(price);
-            it--;
-            cout << (*it).first << endl ;
-            (*it).second--;
-    }
+        long long N;
+        cin >> N;
+        long long arr[N];
+        for(int i=0; i<N; i++){
+                cin>>arr[i];
+        }
+         set<long long> s;
+         s.insert(-1);
+         for(int i=0; i<N; i++){
+                 s.insert(arr[i]);
+                 auto it = s.find(arr[i]);
+                 cout << (*--it) << "\n";
+         }
 
 
 
-    return 0;
+  return 0;
 }
+
+      
---
+++ stl.cpp
@@ -0,0 +1,14 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//         int total;
+//         string str, rev;
+//         cin >> total;
+//         for(int i=1; i<total; i++){
+//               cin >> str;
+//               rev = str;
+//               reverse(rev.begin(), rev.end());
+//               cout << "Case " << i << ":" << (str==rev? "Yes" : "No") << "\n";   
+//         }
+//         return 0;
+// }
